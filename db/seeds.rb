a4599dcac17c4a18e96091d5be6a08f6cbdf8e0f
Mario Stehling <mario.stehling@gmx.de>
2020-11-11 13:45:21 +0100

    Added YELP MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "R1", address: "Street1", category: "chinese")
+Restaurant.create(name: "R2", address: "Street2", category: "italian")
+Restaurant.create(name: "R3", address: "Street3", category: "japanese")
+Restaurant.create(name: "R4", address: "Street4", category: "french")
+Restaurant.create(name: "R5", address: "Street5", category: "belgian")
